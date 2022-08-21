5e7dea93d82e034512c7f7b38f7470b852bc75b2
jackl <jacklambert3@comcast.net>
2022-08-20 17:51:29 -0700

    Added hero

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64263" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52302" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
