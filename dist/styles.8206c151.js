625961effdcebc421cdd8418f2a02c5f2decc8d2
jackl <jacklambert3@comcast.net>
2022-09-16 21:36:40 -0700

    Build for github

--- dist/styles.8206c151.js
+++ dist/styles.8206c151.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\bg-curve-mobile.svg":[["bg-curve-mobile.9bd79edc.svg","images/bg-curve-mobile.svg"],"images/bg-curve-mobile.svg"],"./..\\images\\bg-curve-desktop.svg":[["bg-curve-desktop.48f4b951.svg","images/bg-curve-desktop.svg"],"images/bg-curve-desktop.svg"],"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52302" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50079" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
